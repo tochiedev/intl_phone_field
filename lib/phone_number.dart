c42886c69a8e00ee1df113176179c38ef82a0dfb
tochiedev <tochie.dev@gmail.com>
2022-02-12 19:54:59 +0100

    Added country name propery

--- lib/phone_number.dart
+++ lib/phone_number.dart
@@ -2,17 +2,18 @@ class PhoneNumber {
   String countryISOCode;
   String countryCode;
   String number;
+  String country;
 
   PhoneNumber({
     required this.countryISOCode,
     required this.countryCode,
     required this.number,
+    required this.country,
   });
 
   String get completeNumber {
     return countryCode + number;
   }
 
-  String toString() =>
-      'PhoneNumber(countryISOCode: $countryISOCode, countryCode: $countryCode, number: $number)';
+  String toString() => 'PhoneNumber(countryISOCode: $countryISOCode, countryCode: $countryCode, number: $number)';
 }
